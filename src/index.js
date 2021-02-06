dd246403debde765b7e4190df707b8d4f6ea3517
João Dematé Jr <joaodematejr@gmail.com>
2021-02-06 20:17:49 -0300

    Es Module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+"use strict"
 
 import React from 'react';
-import ReactDom from 'react-dom';
+import { render as reactDomRender } from './react-dom';
 
 import Title from './app';
 
-ReactDom.render(
+reactDomRender(
     React.createElement(Title),
     document.querySelector('[data-js="app"]')
 )
